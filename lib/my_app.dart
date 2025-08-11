afa02291ced38e4b66c43185ffcfb512122f4477
Michele Maione <mikymaione@hotmail.it>
2025-08-11 15:38:34 +0200

    feat: banner debug

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: true,
       title: "WebRTC - [MAIONE MIKΨ]",
       themeMode: ThemeMode.system,
       theme: ThemeData(
